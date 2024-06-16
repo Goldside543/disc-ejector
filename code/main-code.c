1954f1788c9c7794222c5978088b94acd82571b7
Goldside543 <goldside000@outlook.com>
2024-06-16 00:03:41 -0400

    Update main-code.c

--- code/main-code.c
+++ code/main-code.c
@@ -5,7 +5,7 @@ int main(void) {
     // Not really much to say, the code is self explanatory.
     system("eject D:");
     printf("If all went well, your disc has been ejected. If not, do the following and read the error message.")
-    system("pause")
+    system("pause");
     // Below is a bit of code I'm trying to make open the error message file. The filename is temporary, and will be used until I get a proper exe file for it.
     errormessage();
     system("no-disc-error.exe")
