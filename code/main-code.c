9b119199bc39744a551812f66508ac1f8cb23cb1
Goldside543 <goldside000@outlook.com>
2024-06-15 20:27:07 -0400

    Update main-code.c

--- code/main-code.c
+++ code/main-code.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(void) {
     // Not really much to say, the code is self explanatory.
     system("eject D:");
     // Below is a bit of code I'm trying to make open the error message file. The filename is temporary, and will be used until I get a proper exe file for it.
+    errormessage();
     system("no-disc-error.exe")
     return 0;
 }
