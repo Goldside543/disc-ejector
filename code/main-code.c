d69712c0ac85ee1e083783020ff21cf3f4f28df1
Goldside543 <goldside000@outlook.com>
2024-06-16 00:03:58 -0400

    Update main-code.c

--- code/main-code.c
+++ code/main-code.c
@@ -8,6 +8,6 @@ int main(void) {
     system("pause");
     // Below is a bit of code I'm trying to make open the error message file. The filename is temporary, and will be used until I get a proper exe file for it.
     errormessage();
-    system("no-disc-error.exe")
+    system("no-disc-error.exe");
     return 0;
 }
